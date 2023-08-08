f4f25a39b883cbbc0df215b99fada18533071c30
akankshamahind <141725349+akankshamahind@users.noreply.github.com>
2023-08-08 21:21:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,32 +3,29 @@ const retrieveEntries = () =>{
    let entries = localStorage.getItem("userEntries");
    if (entries){
        entries = JSON.parse(entries);
-   }
-  else {
+   }else {
     entries = [];
   }
   return entries;
 }
 let userEntry = retriveEntries();
-
 const displayEntries = () => {
      const entries = retriveEntries();
-   
-  const tableEntries = entries.map((entry) => {
-    const namecell = '<td class='border px-4 py-2'>${entry.name}</td>';
-    const emailCell = '<td class='border px-4 py-2'>${entry.email}</td>';
-    const passwordCell = '<td class='border px-4 py-2'>${entry.password}</td>';
-    const dobCell = '<td class='border px-4 py-2'>${entry.dob}</td>';
-    const acceptedTermsCell = '<td class='border px-4 py-2'>${entry.acceptedTerms}</td>';
-    const row = '<tr>${namecell} ${emailCell} ${passwordCell} ${dobCell} ${acceptedTermsCell}</tr>';
-    return row;
+     const tableEntries = entries.map((entry) =>{
+         const namecell = '<td class="border px-4 py-2">${entry.name}</td>';
+         const emailCell = '<td class="border px-4 py-2">${entry.email}</td>';
+         const passwordCell = '<td class="border px-4 py-2">${entry.password}</td>';
+         const dobCell = '<td class="border px-4 py-2">${entry.dob}</td>';
+         const acceptedTermsCell = '<td class="border px-4 py-2">${entry.acceptedTerms}</td>';
+         const row = '<tr>${namecell} ${emailCell} ${passwordCell} ${dobCell} ${acceptedTermsCell}</tr>';
+         return row;
   }).join("\n");
   const table = `<table class="table-auto w-full"><tr>
   <th class="px-4 py-2">Name</th>
   <th class="px-4 py-2">Email</th>
-  <th class="px-4 py-2">password</th>
-  <th class="px-4 py-2">dob</th>
-  <th class="px-4 py-2">accepted terms</th> 
+  <th class="px-4 py-2">Password</th>
+  <th class="px-4 py-2">Dob</th>
+  <th class="px-4 py-2">Accepted terms</th> 
 </tr>${tableEntries}</table>`;
 
      
@@ -38,12 +35,12 @@ let details = document.getElementById("userEntriesDiv");
 
 const saveUserForm = (event) => {
   event.preventDefault();
-  // Get form values
+  
   const name = documnet.getElementById("name").value;
   const email =  documnet.getElementById("email").value;
   const passsword = documet.getElementById("password").value;
   const dob = documet.getElementById("dob").value;
-  const acceptedTerms = document.getElementById("acceptTerms").checked;
+  const acceptedTerms = document.getElementById("accept-terms").checked;
   const entry = {
     name: name,
     email: email,
@@ -52,7 +49,7 @@ const saveUserForm = (event) => {
     acceptedTerms: acceptedTerms
   }
   userEntry.push(entry);
-  localStorage.setItem("user-entries", JSON.stringify(userEntry));
+  localStorage.setItem("userEntries", JSON.stringify(userEntry));
   displayEntries();
 //     document.getElementById("inline-full-name").value="";
 //     document.getElementById("inline-email").value="";
