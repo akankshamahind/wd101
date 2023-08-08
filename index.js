8bac987045f79090d9586692e56ae5ef4cfa9f3e
akankshamahind <141725349+akankshamahind@users.noreply.github.com>
2023-08-08 19:02:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let userform = document.getElementById("user_form")
 const retrieveEntries = () => {
-   let entries = localStorage.getItem("user");
+   let entries = localStorage.getItem("user-entries");
    if (entries) {
        entries = JSON.parse(entries);
    }
@@ -12,28 +12,28 @@ const retrieveEntries = () => {
 let userEntries = retrieveEntries();
 
 const displayEntries = () => {
-    // const entries = retrieveEntries();
-  const entries = usreEntries;
+     const entries = retrieveEntries();
+   
   const tableEntries = entries.map((entry) => {
-    const namecell = '<td class='border px-4 py-2'>${entry.name}</td>';
-    const emailcell = '<td class='border px-4 py-2'>${entry.email}</td>';
-    const passwordcell = '<td class='border px-4 py-2'>${entry.password}</td>';
-    const dobcell = '<td class='border px-4 py-2'>${entry.password}</td>';
-    const acceptedTermscell = '<td class='border px-4 py-2'>${entry.acceptedTerms}</td>';
-    const row = '<tr>${namecell} ${emailcell} ${passwordcell} ${dobcell} ${acceptedTermscell}</tr>';
+    const name = '<td class='border px-4 py-2'>${entry.name}</td>';
+    const emailCell = '<td class='border px-4 py-2'>${entry.email}</td>';
+    const passwordCell = '<td class='border px-4 py-2'>${entry.password}</td>';
+    const dobCell = '<td class='border px-4 py-2'>${entry.dob}</td>';
+    const acceptedTermsCell = '<td class='border px-4 py-2'>${entry.acceptedTerms}</td>';
+    const row = '<tr>${namecell} ${emailCell} ${passwordCell} ${dobCell} ${acceptedTermsCell}</tr>';
     return row;
   })
   .join("\n");
 
   const table = '<table class="table-auto w-full"><tr>
-
   <th class="px-4 py-2">Name</th>
   <th class="px-4 py-2">Email</th>
   <th class="px-4 py-2">password</th>
-  <th class="px-4 py-2">Dob</th>
-  <th class="px-4 py-2">acceptedTerms</th> 
-    </tr>${tableEntries} </table>";
-    let details = document.getElementById("user");
+  <th class="px-4 py-2">dob</th>
+  <th class="px-4 py-2">accepted terms</th> 
+    </tr>${tableEntries} </table>`;
+     
+let details = document.getElementById("user-entries");
   details.innerHTML = table;
 }
 
@@ -43,35 +43,49 @@ const saveUserFrom = (event) => {
   const name = documet.getElementById("name").value;
   const email =  documet.getElementById("email").value;
   const passsword = documet.getElementById("password").value;
-  const dob = new Date(documet.getElementById("DOB").value);
-  const acceptedTerms = documet.getElementById("acceptedTerms").checked;
-
-  //Validate age between 18 and 55
-  const age = new Date().getFullYear() - dob.getFullYear();
-  if (age < 18 || age > 55) {
-       alert("You must be between 18 and 55 years old to register.");
-       return;
-  }
-
+  const dob = documet.getElementById("dob").value);
+  const acceptedTerms = documet.getElementById("acceptTerms").checked;
   const entry = {
     name,
     email,
     password,
     dob,
-    acceptedTerms
+    acceptTerms
   };
 
-  userEntries = retrieveEntries();
+  
   userEntries.push(entry);
-
-  localStorage.setItem("user", JSON.stringify(userEntries));
+  localStorage.setItem("user-entries", JSON.stringify(userEntries));
   displayEntries();
-  userform.reset();
 }
-userform.addEventListener("submit", saveUserForm)
 
+document.addEventListener("DOMContentLoaded", function () {
+ const form = document.getElementById("user-form");
+ const dobInput.getElementById("dob");
+ function validateAge(dateOfBirth) {
+  const today = new Date();
+  const birthDate = new Date(dateOfBirth);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+  if (
+   monthDiff < 0 ||
+   (monthDiff === 0 && today.getDate() <birthDate.getDate())
+   ) {
+   age--;
+   }
+   return age;
+ }
+
+ function handlesubmit(event) {
+   event.preventDefault();
+   const dateOfBirth = dobInput.value;
+   if(age > 18 && age <55){
+     saveUserForm(event);
+ }
+    else {
+      alert("You must be between 18 and 55 yaer old to register.");
+     }
+   }
+   form.addEventListener("submit", handleSubmit);
 displayEntries();
-
-  
-  
-    
+});
