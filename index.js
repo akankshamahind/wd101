b43b9f9506325c57be5c88b8f495ad6a661f1e91
akankshamahind <141725349+akankshamahind@users.noreply.github.com>
2023-08-08 20:53:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let userform = document.getElementById("user_form")
-const retrieveEntries = () => {
-   let entries = localStorage.getItem("user-entries");
-   if (entries) {
+let userform = document.getElementById("user-form");
+const retrieveEntries = () =>{
+   let entries = localStorage.getItem("userEntries");
+   if (entries){
        entries = JSON.parse(entries);
    }
   else {
@@ -9,10 +9,10 @@ const retrieveEntries = () => {
   }
   return entries;
 }
-let userEntries = retrieveEntries();
+let userEntry = retriveEntries();
 
 const displayEntries = () => {
-     const entries = retrieveEntries();
+     const entries = retriveEntries();
    
   const tableEntries = entries.map((entry) => {
     const namecell = '<td class='border px-4 py-2'>${entry.name}</td>';
@@ -22,29 +22,28 @@ const displayEntries = () => {
     const acceptedTermsCell = '<td class='border px-4 py-2'>${entry.acceptedTerms}</td>';
     const row = '<tr>${namecell} ${emailCell} ${passwordCell} ${dobCell} ${acceptedTermsCell}</tr>';
     return row;
-  })
-  .join("\n");
-
+  }).join("\n");
   const table = `<table class="table-auto w-full"><tr>
   <th class="px-4 py-2">Name</th>
   <th class="px-4 py-2">Email</th>
   <th class="px-4 py-2">password</th>
   <th class="px-4 py-2">dob</th>
   <th class="px-4 py-2">accepted terms</th> 
-    </tr>${tableEntries} </table>`;
+</tr>${tableEntries}</table>`;
+
      
-let details = document.getElementById("user-entries");
+let details = document.getElementById("userEntriesDiv");
   details.innerHTML = table;
 }
 
-const saveUserFrom = (event) => {
-  event.prevenDefault();
+const saveUserForm = (event) => {
+  event.preventDefault();
   // Get form values
   const name = documnet.getElementById("name").value;
   const email =  documnet.getElementById("email").value;
   const passsword = documet.getElementById("password").value;
-  const dob = documet.getElementById("dob").value);
-  const acceptedTerms = documet.getElementById("acceptTerms").checked;
+  const dob = documet.getElementById("dob").value;
+  const acceptedTerms = document.getElementById("acceptTerms").checked;
   const entry = {
     name: name,
     email: email,
@@ -52,12 +51,14 @@ const saveUserFrom = (event) => {
     dob: dob,
     acceptedTerms: acceptedTerms
   }
-
-  
-  userEntries.push(entry);
-  localStorage.setItem("user-entries", JSON.stringify(userEntries));
+  userEntry.push(entry);
+  localStorage.setItem("user-entries", JSON.stringify(userEntry));
   displayEntries();
+//     document.getElementById("inline-full-name").value="";
+//     document.getElementById("inline-email").value="";
+//     document.getElementById("inline-password").value="";
+//     document.getElementById("inline-dob").value="";
+//     document.getElementById("accept-terms").checked=false;
 }
-
 userForm.addEventListener("submit", saveUserForm);
 displayEntries();
